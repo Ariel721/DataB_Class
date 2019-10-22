775575bde093fe6194d3b3b2df66d4ba642f704f
ariel721 <ariel.r.garcia.c@gmail.com>
2019-10-22 15:54:43 -0700

    Select JOIN ORDER BY Clauses

--- Mod03-notes.sql
+++ Mod03-notes.sql
@@ -32,3 +32,24 @@ CustomerID = 'DUMON'
 
 SELECT AVG(UnitPrice) 'Average Sales Price' From [Order Details];
 
+SELECT ProductName FROM Products;
+
+-- Using 2 tables (The Improved ANSI way)      
+SELECT ProductName, CategoryName
+ FROM Products JOIN Categories
+  ON Products.CategoryId = Categories.CategoryId
+
+--(Table Aliases)--
+-- Alias can be used to rename a table names in the query.
+SELECT ProductName, CategoryName
+ FROM Products as P JOIN Categories as C   -- *******Ask instructor*******--
+  ON P.CategoryId = C.CategoryId;
+
+
+-- SELECT Pub_id, Type, Title_id, Price FROM Pubs..Titles
+  --ORDER BY Pub_id DESC, Type, Price;                                  *******Ask instuctor*******
+
+SELECT * FROM [Order Details];
+SELECT OrderID, ProductID, UnitPrice From [Order Details] ORDER BY OrderID DESC,ProductID;
+
+
