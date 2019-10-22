3e86a411fe0a1999806e5c40c98f5e3938440642
ariel721 <ariel.r.garcia.c@gmail.com>
2019-10-22 13:54:43 -0700

    Aliases try

--- Mod03-notes.sql
+++ Mod03-notes.sql
@@ -1,6 +1,34 @@
 use master;
 go
-drop database Ariel_Garcia_Mod2_Lab2;
+--drop database Ariel_Garcia_Mod2_Lab2;
 go
 use Northwind;
 go
+SELECT * From Customers;
+
+SELECT 
+CompanyName,
+ContactName,
+Phone, 
+GETDATE() 
+from Customers 
+Where 
+(CustomerID ='EASTC'
+or
+CustomerID = 'DUMON'
+);
+--with alias
+SELECT 
+CompanyName,
+ContactName,
+Phone, 
+[Date] = GETDATE() --Alias here
+from Customers 
+Where 
+(CustomerID ='EASTC'
+or
+CustomerID = 'DUMON'
+);
+
+SELECT AVG(UnitPrice) 'Average Sales Price' From [Order Details];
+
