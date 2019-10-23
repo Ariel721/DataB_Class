ba5be41e093101e23a8f2b011baec80427eae1b4
ariel721 <ariel.r.garcia.c@gmail.com>
2019-10-22 19:59:13 -0700

    Aggregate Functions

--- Mod03-notes.sql
+++ Mod03-notes.sql
@@ -50,6 +50,32 @@ SELECT ProductName, CategoryName
   --ORDER BY Pub_id DESC, Type, Price;                                  *******Ask instuctor*******
 
 SELECT * FROM [Order Details];
-SELECT OrderID, ProductID, UnitPrice From [Order Details] ORDER BY OrderID DESC,ProductID;
+SELECT OrderID, ProductID, UnitPrice From [Order Details] ORDER BY OrderID ASC,ProductID;
 
+SELECT ProductName, [Quantity Sum] = Sum(Quantity) --< Does this 3rd and 5th 
+FROM[Order Details] JOIN Products --< Does this 1st  
+ ON [Order Details].ProductID = Products.ProductID
+Where Quantity > 10 --< Does this 2nd
+Group By ProductName --< Does this 3rd
+  Having Sum(Quantity) > 200 --< Does this 4th, after the aggregation
+Order By ProductName;
+
+SELECT * FROM [Order Details] Order by Quantity;
+
+USE pubs
+Select [Date] = GetDate(), [Price]=IsNull(Price, 0), Title
+ From Pubs.dbo.Titles;
+
+ Select 
+  Cast(GetDate() as Date) as [TodaysDate]
+, IsNull(Price, 0) as [CurrentPrice]
+, IsNull(Cast(Price as varchar(50)), 'Not For Sale!') as [CurrentPriceAsTEXT]
+, Title
+ From Pubs.dbo.Titles
+Go
+
+SELECT Stor_id, Title_id, SUM(qty) AS 'Quantity'
+  FROM Pubs.dbo.Sales
+  GROUP BY Stor_id,Title_id
+    WITH Cube 
 
